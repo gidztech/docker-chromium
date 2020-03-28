e2926c4b921551b27957714488da00434cf17f67
Filip Szczesniak <fiszczu@gmail.com>
2020-03-28 15:59:53 +0100

    #8 Refactor chromium revision argument and tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    silent: true
+    silent: true,
+    resetMocks: true
 };
--- tests/run-container/run-container.test.js
+++ tests/run-container/run-container.test.js
@@ -1,33 +1,17 @@
-const path = require('path');
 const {
     dockerSetChromiumConfig,
     dockerRunChromium
 } = require('../../lib/index');
 
-const dockerComposePath = path.join(__dirname, './config/docker-compose.yml');
-const dockerFilePath = path.join(__dirname, './config/Dockerfile');
-
 describe('runContainer', async () => {
     it('runs container and provides websocket uri', async () => {
-        // setup getConfig mock in config module
-        jest.mock('../../src/config');
-        const config = require('../../src/config');
-
-        config.getConfig.mockImplementation(() => ({
-            dockerComposePath,
-            dockerFilePath
-        }));
-
-        jest.setTimeout(300000); // give it 5 minutes to download image/run container
-
-        // app code
-        await dockerSetChromiumConfig({
-            flags: [' -–ignore-certificate-errors']
+        dockerSetChromiumConfig({
+            flags: [' -–ignore-certificate-errors'],
+            revision: 754306
         });
 
         const webSocketUri = await dockerRunChromium();
 
-        // assertions
         expect(webSocketUri).toContain('ws://');
-    });
+    }, 300000);
 });
